5736f44366a0eb4925230288a072f608e4ae1991
James DeVore <jdevore4592@gmail.com>
2024-07-02 12:15:06 -0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,28 +5,20 @@ cd ~
 mkdir -p ~/.config/nvim ~/.config/alacritty
 
 # install all the things
-
 sudo apt update
 sudo apt install build-essential clang git stow alacritty neovim nodejs wget
-
 # clone down dotfiles
 git clone https://github.com/JamesDeVore/dotfiles.git
-
 # start the stow process
 cd dotfiles/.config
-
 stow nvim -t ~/.config/nvim
 stow alacritty -t ~/.config/alacritty/
 cd ~
-
 # Download the Cousine Nerd Font
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Cousine.zip -O Cousine.zip
-
 # Create a directory for local fonts
 mkdir -p ~/.local/share/fonts
-
 # Extract the font to the fonts directory
 unzip Cousine.zip -d ~/.local/share/fonts
-
 # Refresh the font cache
 fc-cache -fv
